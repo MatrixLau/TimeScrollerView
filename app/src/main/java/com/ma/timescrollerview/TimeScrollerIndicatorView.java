168f0353575ea8a8ebca17761c729d84ab2afa8c
MatrixLau <matrixlj0113@gmail.com>
2023-08-11 17:39:06 +0800

    Update TimeScrollerIndicatorView

--- app/src/main/java/com/ma/timescrollerview/TimeScrollerIndicatorView.java
+++ app/src/main/java/com/ma/timescrollerview/TimeScrollerIndicatorView.java
@@ -1,10 +1,12 @@
 package com.ma.timescrollerview;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
+import android.util.TypedValue;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -16,7 +18,10 @@ public class TimeScrollerIndicatorView extends View {
     Paint indicatorPaint;
 
     int indicatorColor = Color.BLUE;
-    float indicatorStrokeWidth = 50f;
+    float indicatorStrokeWidth = getDp(10);
+    float widthPerMin = 0f;
+    int hour = 2;
+    int min = 19;
 
     public TimeScrollerIndicatorView(Context context) {
         super(context);
@@ -25,6 +30,10 @@ public class TimeScrollerIndicatorView extends View {
 
     public TimeScrollerIndicatorView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TimeScrollerIndicatorView);
+        indicatorStrokeWidth = typedArray.getDimension(R.styleable.TimeScrollerIndicatorView_TimeScrollerIndicatorView_stroke_width, indicatorStrokeWidth);
+        typedArray.recycle();
         init();
     }
 
@@ -57,7 +66,31 @@ public class TimeScrollerIndicatorView extends View {
         int width = getWidth() - paddingLeft - paddingRight;
         int height = getHeight() - paddingTop - paddingBottom;
 
-        canvas.drawLine(paddingLeft, paddingTop, paddingLeft, paddingTop + height, indicatorPaint);
+        widthPerMin = (float) width / 1440;
 
+        float additionWidth = 0f;
+
+        if (hour >= 0 && min >= 0) {
+            additionWidth += ((hour * 60) + min) * widthPerMin;
+        }
+
+        canvas.drawLine(paddingLeft + additionWidth, paddingTop, paddingLeft + additionWidth, paddingTop + height, indicatorPaint);
+
+    }
+
+    public void setTime(int hour, int min) {
+        this.hour = hour;
+        this.min = min;
+        invalidate();
+    }
+
+    /**
+     * 获取统一化像素大小
+     *
+     * @param dp 传入的值
+     * @return 转化成统一标准的值
+     */
+    public float getDp(int dp) {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, getResources().getDisplayMetrics());
     }
 }
